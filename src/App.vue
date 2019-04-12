08dde63ed427476d3754bdb4c37ea3663b3421d1
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-04-12 01:28:33 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="app">
+  <div class="titles">
+    <h1 class="title-1">{{ titleApp }}</h1>
+    <h2 class="title-2">{{ subtitleApp }}</h2>
+  </div>
+  <login-form></login-form>
+</div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm';
+
+export default {
+  name: 'app',
+  components: {
+    LoginForm
+  },
+  data(){
+    return {
+      titleApp: 'SSAL',
+      subtitleApp: 'Sistema de reservación de salas'
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+@import 'assets/scss/main.scss';
+
+.titles{
+  margin: 20px 0;
+  padding: 10px;
+}
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="login">
+        <form action="" v-on:submit.prevent="enviarDatos" id="form-login">
+            <div class="form-inputs">
+                <label for="username" class="form-label">USERNAME:</label>
+                <input v-model="username" type="text" name="username" id="username" class="form-input">
+                <label for="password" class="form-label">PASSWORD:</label>
+                <input v-model="password" type="password" name="password" id="password" class="form-input">   
+            </div>
+            <div v-if="infoLoginCompleta" class="form-botones">
+                <input type="submit" value="Ingresar" class="form-boton">
+                <input type="reset" value="Limpiar" class="form-boton">
+            </div>  
+        </form>
+        <pre class="info">
+        {{ $data }}
+        </pre>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+
+  data() {
+      return {
+        username: '',
+        password: ''
+      }
+  }, 
+
+  methods: {
+      enviarDatos: function(){
+          alert("Enviando los datos...");
+          this.username = '';
+          this.password = '';
+      }
+  }, 
+
+   computed: {
+        infoLoginCompleta: function () {
+            if (this.username && this.password){
+                return true;
+            }
+            return false;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.info{
+    background-color: red;
+}
+</style>
+
